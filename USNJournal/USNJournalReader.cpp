27793cb0ecd9c50633f40e8c97ba03641b633402
Luis Ceja <lcejaqui@gmail.com>
2026-02-26 01:22:32 -0800

    QOL changes. Rewrote While dwRetBytes logic; now gets the next UsnRecord immediately and uses the CurrentUsnRecord. Added FILE_FLAG_OVERLAPPED flag to allow asynchronous polling. Allows using continue for cleaner logic. Removed {} on various single-line if-statements. Added DEGUG_PAUSE() function and moved library requirements in Ifdef statement. Changed BUF_LEN to 64*1024 for readability (64 kB). Changes fatal output to cerr. Removed extra check for '.' in isXml().

--- USNJournal/USNJournalReader.cpp
+++ USNJournal/USNJournalReader.cpp
@@ -1,25 +1,27 @@
-
-#include <stdlib.h>
 #include <Windows.h>
 #include <WinIoCtl.h>
 #include <iostream>
 #include <stdio.h>
 #include <sstream>
 
-#define BUF_LEN 65536 //64kB
+#ifdef _DEBUG
+#include <cstdlib>
+#define DEBUG_PAUSE() std::system("pause")
+#else
+#define DEBUG_PAUSE() ((void)0)
+#endif
+
+#define BUF_LEN 64 * 1024 //64kB
 
 // Why do I need this pragma comment?
 #pragma comment(lib, "Advapi32.lib") 
-
 
 bool IsProcessElevated()
 {
     HANDLE token = nullptr;
     if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &token))
-    {
         return false;
-    }
-
+    
     TOKEN_ELEVATION elevation;
     DWORD size;
     BOOL result = GetTokenInformation(token, TokenElevation, &elevation, sizeof(elevation), &size);
@@ -30,21 +32,20 @@ bool IsProcessElevated()
 
 void fatal(const std::string& msg, int status = 1)
 {
-    std::cout << msg << "\n";
-    system("pause");
+    std::cerr << msg << "\n";
+    DEBUG_PAUSE();
     std::exit(status);
 }
 
 bool isXml(const WCHAR* name, int len) {
     // Minimum length: ".xml" = 4 chars
-    if (len < 4)
+    if (len < 4) 
         return false;
 
-    // Compare last 4 characters
-    return (name[len - 4] == L'.' &&
-        (name[len - 3] == L'x' || name[len - 3] == L'X') &&
-        (name[len - 2] == L'm' || name[len - 2] == L'M') &&
-        (name[len - 1] == L'l' || name[len - 1] == L'L'));
+    // Compare last 3 characters
+    return (name[len - 3] == L'x' || name[len - 3] == L'X') &&
+           (name[len - 2] == L'm' || name[len - 2] == L'M') &&
+           (name[len - 1] == L'l' || name[len - 1] == L'L');
 }
 
 int main()
@@ -56,7 +57,7 @@ int main()
     CHAR Buffer[BUF_LEN];
 
     USN_JOURNAL_DATA JournalData;
-    PUSN_RECORD_V3 UsnRecord;
+    PUSN_RECORD_V3 UsnRecord, CurrentUsnRecord;
     DWORD dwBytes, dwRetBytes;
 
     READ_USN_JOURNAL_DATA_V1 ReadData = { 0 };
@@ -64,30 +65,22 @@ int main()
     ReadData.ReturnOnlyOnClose = FALSE;
     ReadData.MaxMajorVersion = 3;
 
-    hVol = CreateFile( TEXT("\\\\.\\c:"), GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+    hVol = CreateFile( TEXT("\\\\.\\c:"), GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, NULL);
 
     if (hVol == INVALID_HANDLE_VALUE)
-    {
         fatal("CreateFile failed (" + std::to_string(GetLastError()) + ")");
-    }
+    
 
     if (!DeviceIoControl( hVol, FSCTL_QUERY_USN_JOURNAL, NULL, 0, &JournalData, sizeof(JournalData), &dwBytes, NULL))
-    {
         fatal("Query journal failed (" + std::to_string(GetLastError()) + ")");
-    }
-
+    
     ReadData.UsnJournalID = JournalData.UsnJournalID;
-    //ReadData.StartUsn = JournalData.FirstUsn;
     ReadData.StartUsn = JournalData.NextUsn;
 
     while(true)
     {
-        memset(Buffer, 0, BUF_LEN);
-
         if (!DeviceIoControl( hVol, FSCTL_READ_USN_JOURNAL, &ReadData, sizeof(ReadData), &Buffer, BUF_LEN, &dwBytes, NULL))
-        {
-            fatal("Read journal failed (" + std::to_string(GetLastError()) + ")");
-        }
+            fatal("Read journal failed (" + std::to_string(GetLastError()) + ")"); 
 
         dwRetBytes = dwBytes - sizeof(USN);
 
@@ -96,31 +89,34 @@ int main()
 
         while (dwRetBytes > 0)
         {
-            // Filters Out Active and Temporary Files
-            if ((UsnRecord->Reason & USN_REASON_CLOSE ) && !(UsnRecord->FileName[0] == L'$'))
-            {
-                // Filters XML files Only
-                if (isXml(UsnRecord->FileName, UsnRecord->FileNameLength / 2))
-                {
-                    //Only Interested In XML Files
-                    printf("USN: %I64x\n", UsnRecord->Usn);
-                    printf("File name: %.*S\n", UsnRecord->FileNameLength / 2, UsnRecord->FileName);
-                    printf("Reason: %x\n", UsnRecord->Reason);
-                    printf("\n");
-                }                
-            }
-
+            CurrentUsnRecord = UsnRecord;
             dwRetBytes -= UsnRecord->RecordLength;
-
-            // Find the next record
             UsnRecord = (PUSN_RECORD_V3)(((PCHAR)UsnRecord) + UsnRecord->RecordLength);
+
+            // Filters Active Files
+            if (!(CurrentUsnRecord->Reason & USN_REASON_CLOSE))
+                continue;
+
+            // Filters Temporary Files
+            if (CurrentUsnRecord->FileName[0] == L'$')
+                continue;
+
+            // Filters XML files Only
+            if (!isXml(CurrentUsnRecord->FileName, CurrentUsnRecord->FileNameLength / 2))
+                continue;
+                
+            // Process Logic
+            printf("USN: %I64x\n", CurrentUsnRecord->Usn);
+            printf("File name: %.*S\n", CurrentUsnRecord->FileNameLength / 2, CurrentUsnRecord->FileName);
+            printf("Reason: %x\n", CurrentUsnRecord->Reason);
+            printf("\n");
         }
+
         // Update starting USN for next call
         ReadData.StartUsn = *(USN*)&Buffer;
     }
 
     CloseHandle(hVol);
-
-    system("pause");
+    DEBUG_PAUSE();
     return 0;
 }
